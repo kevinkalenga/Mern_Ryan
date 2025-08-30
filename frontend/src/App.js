6eae01fbfc76cf790ea08008c39f3f11b6a86407
kevin kalenga <kevinkalenga10@gmail.com>
2025-08-30 02:26:45 +0200

    Install react-router-dom for routes and create pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 const App = () => {
   return (
-    <div>
-      <p>React App</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
